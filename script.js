76fb459963efa0dc385a984993c9402e4ff721d0
GxG75 <gaelgarciaangeles75@gmail.com>
2024-07-24 13:11:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,112 @@
 const btn_plot = document.getElementById("btn_plot");
 btn_plot.addEventListener("click", plot);
-
-async function load_years(){
-    console.log('cambio', station.value);
-    var year = await get_year(station.value);
-    for (y of year){
-        var opt = document.createElement('option');
-        opt.value = y;
-        opt.innerHTML = y;
-        sel_year.appendChild(opt);
-    }
-}
-
-async function load_months(){
-    console.log('cambio', sel_year.value);
-    var months = await get_months(station.value, sel_year.value);
-    console.log(months);
-    for (m of months){
-        var opt = document.createElement('option');
-        opt.value = m;
-        opt.innerHTML = m;
-        sel_month.appendChild(opt);
-    }
-}
-
-async function load_dates(){
-    console.log('cambio', sel_month.value);
-    var dates = await get_days(station.value, sel_month.value);
-    console.log(dates);
-    for (d of dates){
-        var opt = document.createElement('option');
-        opt.value = d;
-        opt.innerHTML = d;
-        sel_fecha.appendChild(opt);
-    }
-}
-
 const station = document.getElementById("plantel");
-station.addEventListener("change", load_years);
-
 const sel_year = document.getElementById("year");
-sel_year.addEventListener("change", load_months);
-
 const sel_month = document.getElementById("mes");
-sel_month.addEventListener("change", load_dates);
-
 const sel_fecha = document.getElementById("Fecha");
-//station.addEventListener("change", load_years);
+let currentStep = 1; 
+
+hideButtons();
+
+function hideButtons() {
+  station.style.display = "none";
+  sel_year.style.display = "none";
+  sel_month.style.display = "none";
+  sel_fecha.style.display = "none";
+  btn_plot.style.display = "none";
+  document.getElementById("btn_download").style.display = "none";
+
+  switch (currentStep) {
+    case 1:
+      station.style.display = "block";
+      station.disabled = false; 
+      break;
+    case 2:
+      station.style.display = "block";
+      sel_year.style.display = "block";
+      station.disabled = true; 
+      sel_year.disabled = false; 
+      break;
+    case 3:
+      station.style.display = "block";
+      sel_year.style.display = "block";
+      sel_month.style.display = "block";
+      station.disabled = true; 
+      sel_year.disabled = true; 
+      sel_month.disabled = false; 
+      break;
+    case 4:
+      station.style.display = "block";
+      sel_year.style.display = "block";
+      sel_month.style.display = "block";
+      sel_fecha.style.display = "block";
+      station.disabled = true; 
+      sel_year.disabled = true; 
+      sel_month.disabled = true; 
+      sel_fecha.disabled = false; 
+      break;
+    case 5:
+      station.style.display = "block";
+      sel_year.style.display = "block";
+      sel_month.style.display = "block";
+      sel_fecha.style.display = "block";
+      station.disabled = true; 
+      sel_year.disabled = true;
+      sel_month.disabled = true; 
+      sel_fecha.disabled = true; 
+      btn_plot.style.display = "block";
+      document.getElementById("btn_download").style.display = "block";
+      break;
+  }
+}
+
+async function load_years() {
+  console.log('cambio', station.value);
+  var year = await get_year(station.value);
+  for (y of year) {
+    var opt = document.createElement('option');
+    opt.value = y;
+    opt.innerHTML = y;
+    sel_year.appendChild(opt);
+  }
+  currentStep = 2;
+  hideButtons();
+}
+
+async function load_months() {
+  console.log('cambio', sel_year.value);
+  var months = await get_months(station.value, sel_year.value);
+  console.log(months);
+  for (m of months) {
+    var opt = document.createElement('option');
+    opt.value = m;
+    opt.innerHTML = m;
+    sel_month.appendChild(opt);
+  }
+  currentStep = 3;
+  hideButtons();
+}
+
+async function load_dates() {
+  console.log('cambio', sel_month.value);
+  var dates = await get_days(station.value, sel_month.value);
+  console.log(dates);
+  for (d of dates) {
+    var opt = document.createElement('option');
+    opt.value = d;
+    opt.innerHTML = d;
+    sel_fecha.appendChild(opt);
+  }
+  currentStep = 4;
+  hideButtons();
+}
+
+station.addEventListener("change", load_years);
+sel_year.addEventListener("change", load_months);
+sel_month.addEventListener("change", load_dates);
+sel_fecha.addEventListener("change", () => {
+  currentStep = 5;
+  hideButtons();
+});
+
+hideButtons();
